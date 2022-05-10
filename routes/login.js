32f302bbd05e0ff127cdecda4a6ea173b704a65a
ericchi00 <ericchi00@gmail.com>
2022-05-10 15:10:39 -0700

    added authenticate

--- routes/login.js
+++ routes/login.js
@@ -1,9 +1,20 @@
 import express from 'express';
+import passport from 'passport';
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
 	res.render('login', { title: 'Login Form' });
 });
+
+router.post(
+	'/',
+	passport.authenticate('local', {
+		successRedirect: '/',
+		failureRedirect: '/login',
+	})
+);
+
+
 
 export default router;
