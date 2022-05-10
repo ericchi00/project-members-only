3270be0833c02208c779e0c31dbdb95a8c953cb3
ericchi00 <ericchi00@gmail.com>
2022-05-10 14:26:01 -0700

    created user & mesage models

---
+++ models/message.js
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose';
+
+const { Schema } = mongoose.Schema;
+
+const MessageSchema = new Schema({
+	username: { type: Schema.Types.ObjectID, ref: 'User' },
+	title: { type: String, maxLength: 50 },
+	message: { type: String, required: true, minLength: 1 },
+});
+
+export default mongoose.model('Message', MessageSchema);
