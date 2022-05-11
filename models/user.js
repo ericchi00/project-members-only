b0b826981bfaad6e6353c2777af3aada5364299f
ericchi00 <ericchi00@gmail.com>
2022-05-11 13:39:13 -0700

    added in member and admin

--- models/user.js
+++ models/user.js
@@ -5,6 +5,8 @@ const { Schema } = mongoose;
 const UserSchema = new Schema({
 	username: { type: String, required: true },
 	password: { type: String, required: true },
+	member: { type: Boolean, default: false },
+	admin: { type: Boolean, default: false },
 });
 
 export default mongoose.model('User', UserSchema);
