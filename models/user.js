eee4b30802d4e28ec200cbb2ac56efcb9580ece8
ericchi00 <ericchi00@gmail.com>
2022-05-11 16:35:48 -0700

    added in minlength & join date

--- models/user.js
+++ models/user.js
@@ -3,10 +3,11 @@ import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
 const UserSchema = new Schema({
-	username: { type: String, required: true },
-	password: { type: String, required: true },
+	username: { type: String, required: true, minlength: 4 },
+	password: { type: String, required: true, minlength: 5 },
 	member: { type: Boolean, default: false },
 	admin: { type: Boolean, default: false },
+	join_date: { type: Date, default: Date.now },
 });
 
 export default mongoose.model('User', UserSchema);
