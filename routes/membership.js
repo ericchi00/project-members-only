0c47beea7c174d23367dabcc292abf794c6663a6
ericchi00 <ericchi00@gmail.com>
2022-05-12 13:45:57 -0700

    added in post routes

--- routes/membership.js
+++ routes/membership.js
@@ -1,9 +1,12 @@
 import express from 'express';
+import { membershipPost } from '../controllers/usercontroller.js';
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
 	res.render('membership', { user: res.locals.currentUser });
 });
+
+router.post('/', membershipPost);
 
 export default router;
