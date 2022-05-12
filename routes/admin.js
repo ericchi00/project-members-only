55ed9b20826e306b6c999a9c463004ef4039474f
ericchi00 <ericchi00@gmail.com>
2022-05-12 13:45:50 -0700

    added in post route

--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,12 @@
 import express from 'express';
+import { adminPost } from '../controllers/usercontroller.js';
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
 	res.render('admin', { user: res.locals.currentUser });
 });
+
+router.post('/', adminPost);
 
 export default router;
