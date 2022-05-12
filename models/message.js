882690754e1e4f8250f61debdf6dc548b361be40
ericchi00 <ericchi00@gmail.com>
2022-05-12 15:52:53 -0700

    added virtual function

--- models/message.js
+++ models/message.js
@@ -1,12 +1,20 @@
+import { format, formatDistanceToNow } from 'date-fns';
 import mongoose from 'mongoose';
 
 const { Schema } = mongoose;
 
 const MessageSchema = new Schema({
 	username: { type: Schema.Types.ObjectID, ref: 'User' },
-	title: { type: String, maxLength: 50 },
+	title: { type: String, maxLength: 50, minLength: 1 },
 	message: { type: String, required: true, minLength: 1 },
 	timestamp: { type: Date, default: Date.now },
+});
+
+MessageSchema.virtual('formatDate').get(function () {
+	return formatDistanceToNow(this.timestamp, {
+		includeSeconds: true,
+		addSuffix: true,
+	});
 });
 
 export default mongoose.model('Message', MessageSchema);
